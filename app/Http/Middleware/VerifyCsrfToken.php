0bef388e96eacedf2fa3120ebc0ea28cde54affb
mac-bleck <mauricio.bleck7@gmail.com>
2020-05-01 05:20:46 -0300

    add cors ao laravel

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,10 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'stripe/*',
+        'http://localhost:8000/*',
+        'http://127.0.0.1:8000/*',
+        'http://localhost:3000/*',
+        'http://127.0.0.1:3000/*',
     ];
 }
